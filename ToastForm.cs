325aca37933533255dfe508a7b1d72d3dc389229
ExcitedElectrn <excitedelectrn@gmail.com>
2026-02-20 12:38:24 +0530

    Refine toast with translucent liquid-glass styling

--- ToastForm.cs
+++ ToastForm.cs
@@ -18,11 +18,11 @@ namespace ToastMessageWhenCopied
             this.ShowInTaskbar = false;
             this.TopMost = true;
             this.DoubleBuffered = true;
-            this.Width = 220;
-            this.Height = 52;
+            this.Width = 228;
+            this.Height = 56;
             this.Padding = new Padding(0);
             this.BackColor = Color.Black;
-            this.Opacity = 0.96;
+            this.Opacity = 0.88;
 
             timer = new Timer { Interval = 2200 };
             timer.Tick += (s, e) =>
@@ -62,40 +62,85 @@ namespace ToastMessageWhenCopied
             Rectangle rect = new Rectangle(1, 1, this.Width - 2, this.Height - 2);
 
             using (GraphicsPath path = RoundedRect(rect, 18))
-            using (LinearGradientBrush backgroundBrush = new LinearGradientBrush(
-                rect,
-                Color.FromArgb(242, 79, 70, 229),
-                Color.FromArgb(242, 45, 176, 248),
-                LinearGradientMode.Horizontal))
-            using (Pen borderPen = new Pen(Color.FromArgb(165, 255, 255, 255), 1.2f))
             {
-                g.FillPath(backgroundBrush, path);
-                g.DrawPath(borderPen, path);
+                DrawLiquidGlassBackground(g, rect, path);
+                DrawGlassHighlight(g, rect, path);
+                using (Pen borderPen = new Pen(Color.FromArgb(145, 255, 255, 255), 1.1f))
+                {
+                    g.DrawPath(borderPen, path);
+                }
             }
 
             DrawCheckIcon(g, rect);
 
-            Rectangle textRect = new Rectangle(52, 0, rect.Width - 56, rect.Height);
+            Rectangle textRect = new Rectangle(54, 0, rect.Width - 58, rect.Height);
             using (StringFormat sf = new StringFormat
             {
                 Alignment = StringAlignment.Near,
                 LineAlignment = StringAlignment.Center
             })
-            using (SolidBrush textBrush = new SolidBrush(Color.White))
+            using (SolidBrush textBrush = new SolidBrush(Color.FromArgb(245, 255, 255, 255)))
             using (Font textFont = new Font("Segoe UI Semibold", 10.2f, FontStyle.Regular))
             {
                 g.DrawString(message, textFont, textBrush, textRect, sf);
             }
         }
 
+        private void DrawLiquidGlassBackground(Graphics g, Rectangle rect, GraphicsPath path)
+        {
+            using (LinearGradientBrush glassBrush = new LinearGradientBrush(
+                rect,
+                Color.FromArgb(120, 180, 214, 255),
+                Color.FromArgb(95, 197, 178, 255),
+                LinearGradientMode.ForwardDiagonal))
+            {
+                g.FillPath(glassBrush, path);
+            }
+
+            Rectangle innerRect = new Rectangle(rect.X + 2, rect.Y + 2, rect.Width - 4, rect.Height - 4);
+            using (GraphicsPath innerPath = RoundedRect(innerRect, 16))
+            using (LinearGradientBrush innerBrush = new LinearGradientBrush(
+                innerRect,
+                Color.FromArgb(60, 255, 255, 255),
+                Color.FromArgb(30, 255, 255, 255),
+                LinearGradientMode.Vertical))
+            {
+                g.FillPath(innerBrush, innerPath);
+            }
+        }
+
+        private void DrawGlassHighlight(Graphics g, Rectangle rect, GraphicsPath path)
+        {
+            Rectangle topGlowRect = new Rectangle(rect.X + 6, rect.Y + 4, rect.Width - 12, rect.Height / 2);
+            using (GraphicsPath glowPath = RoundedRect(topGlowRect, 12))
+            using (LinearGradientBrush glowBrush = new LinearGradientBrush(
+                topGlowRect,
+                Color.FromArgb(120, 255, 255, 255),
+                Color.FromArgb(8, 255, 255, 255),
+                LinearGradientMode.Vertical))
+            {
+                Region previousClip = g.Clip;
+                g.SetClip(path);
+                g.FillPath(glowBrush, glowPath);
+                g.Clip = previousClip;
+            }
+        }
+
         private void DrawCheckIcon(Graphics g, Rectangle rect)
         {
-            Rectangle iconBounds = new Rectangle(14, (rect.Height - 24) / 2, 24, 24);
+            Rectangle iconBounds = new Rectangle(16, (rect.Height - 24) / 2, 24, 24);
 
-            using (SolidBrush iconBrush = new SolidBrush(Color.FromArgb(78, 255, 255, 255)))
+            using (LinearGradientBrush iconBrush = new LinearGradientBrush(
+                iconBounds,
+                Color.FromArgb(120, 255, 255, 255),
+                Color.FromArgb(55, 220, 240, 255),
+                LinearGradientMode.Vertical))
+            using (Pen iconBorder = new Pen(Color.FromArgb(120, 255, 255, 255), 1f))
             using (Pen checkPen = new Pen(Color.White, 2.6f))
             {
                 g.FillEllipse(iconBrush, iconBounds);
+                g.DrawEllipse(iconBorder, iconBounds);
+
                 checkPen.StartCap = LineCap.Round;
                 checkPen.EndCap = LineCap.Round;
 
